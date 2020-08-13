3e1df43492ea30d7f5982cbc5229a1a5d8719e97
shiro <shiro.mns@gmail.com>
2020-08-13 16:55:03 +0900

    [追加]キャラクター名一覧、固有名詞一覧、更新完了チェックデータ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :proper_names
+  resources :names
+  resources :uploaded_checks
   get 'top_page/privacy'
   root 'top_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
